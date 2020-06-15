92bbaa5c02ade5f3b86bcf8031d5b18029670647
kikeyama <ikeyama.x.knhk@gmail.com>
2020-06-15 15:05:11 +0900

    add Spring endpoint

--- env_config.py
+++ env_config.py
@@ -7,7 +7,10 @@ db_password = os.environ.get('DB_PASSWORD', 'password')
 db_name = os.environ.get('DB_NAME', 'defaultdb')
 
 # AWS API Gateway settings
-api_fqdn = os.environ.get('API_FQDN', 'xxxxxxxx.execute-api.us-east-1.amazonaws.com')
+apigw_fqdn = os.environ.get('APIGW_FQDN', 'xxxxxxxx.execute-api.us-east-1.amazonaws.com')
+
+# Java Spring settings
+spring_host = os.environ.get('SPRING_HOST', 'localhost:8080')
 
 # SignalFx settings
 ORG_TOKEN = os.environ.get('ORG_TOKEN', '')
--- flask_demo.py
+++ flask_demo.py
@@ -76,33 +76,67 @@ def post_endpoint():
     app.logger.info('posting message: ' + flask_request.form['message'])
     return flask_request.form['message']
 
+## /api/lambda?type=proxy|non-proxy
 @app.route('/api/lambda')
 def lambda_endpoint():
     app.logger.info('getting lambda endpoint')
-    fqdn = env_config.api_fqdn
-    q = {'TableName': 'kikeyama-dynamodb'}
-#    r = requests.get('https://%s/demo/python' %fqdn, params=q)
+    apigw_fqdn = env_config.apigw_fqdn
+    lambda_q = {'TableName': 'kikeyama-dynamodb'}
+    api_type = flask_request.args.get('type', str)
+    lambda_response = ''
+    if api_type == 'proxy':
+        r = requests.get('https://%s/demo/proxy-integration' %apigw_fqdn, params=lambda_q)
+        dict_r = json.loads(r.text)
+        if dict_r['ResponseMetadata']['HTTPStatusCode'] == 200:
+            lambda_response = dict_r
+            app.logger.info('SUCCESS')
+    elif api_type == 'non-proxy':
+        r = requests.get('https://%s/demo/non-proxy-integration' %apigw_fqdn, params=lambda_q)
+        dict_r = json.loads(r.text)
+        if dict_r['statusCode'] == '200':
+            lambda_response = dict_r['body']
+            app.logger.info('SUCCESS')
+    else:
+        r = requests.get('https://%s/demo/python' %apigw_fqdn, params=lambda_q)
+        # Return Error intentionally
+        raise Exception
+#    r = requests.get('https://%s/demo/python' %apigw_fqdn, params=lambda_q)
 #    dict_r = json.loads(r.text)
 #    if dict_r['ResponseMetadata']['HTTPStatusCode'] == 200:
 #        app.logger.info('lambda call: Returned ' + str(dict_r['Count']) + ' results with RequestId: ' + dict_r['ResponseMetadata']['RequestId'])
 #    return 'Lambda Traces'
 
     ## For API Gateway without Proxy Integraion testing
-    r = requests.get('https://%s/demo/non-proxy-integration' %fqdn, params=q)
-    dict_r = json.loads(r.text)
-    if dict_r['statusCode'] == 200:
-        app.logger.info('SUCCESS')
-    return 'Lambda Traces'
+#    r = requests.get('https://%s/demo/non-proxy-integration' %apigw_fqdn, params=lambda_q)
+#    dict_r = json.loads(r.text)
+#    if dict_r['statusCode'] == 200:
+#        app.logger.info('SUCCESS')
+#    return dict_r['body']
+#    return 'Lambda Traces'
     ##
+    return lambda_response
 
 @app.route('/api/lambda/jaeger')
 def lambda_jaeger_endpoint():
     app.logger.info('getting lambda jaeger demo endpoint')
-    fqdn = env_config.api_fqdn
-    r = requests.get('https://%s/demo/jaeger-sample' %fqdn)
+    apigw_fqdn = env_config.apigw_fqdn
+    r = requests.get('https://%s/demo/jaeger-sample' %apigw_fqdn)
     dict_r = json.loads(r.text)
     app.logger.info('lambda call: Result: ' + dict_r['result'] + ' | Trace ID: ' + dict_r['trace_id'])
     return 'Lambda Jaeger Traces'
+
+## /api/spring?status=success|error
+@app.route('/api/spring')
+def spring_endpoint():
+    app.logger.info('getting java spring endpoint')
+    spring_host = env_config.spring_host
+    spring_status = flask_request.args.get('status', str)
+    spring_q = {'status': spring_status}
+
+    r = requests.get('http://%s/api/demo' %spring_host, params=spring_q)
+
+    app.logger.info('spring call: Result: ' + r.text)
+    return 'Spring Trace'
 
 
 if __name__ == '__main__':
